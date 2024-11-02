6148a69a6004e788458fc56992f7be3dfbde3a97
Grubre <ogrodowczykjakub@gmail.com>
2024-11-02 23:15:25 +0100

    Main: Adjust the testing program

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
 #include <string_view>
+#include "error.hpp"
 #include "lexer/lexer.hpp"
 #include "parser/parser.hpp"
 
 auto main() -> int {
     std::cout << "Hello World!" << std::endl;
 
-    const std::string source = R"({"key": "abc", "key2": 123})";
+    const std::string source = R"({"key": {"key2": "value"}})";
     auto [tokens, errors] = jp::collect_tokens(source);
 
+    for (const auto &error : errors) {
+        display_error(error);
+    }
+
+    if (!errors.empty()) {
+        return 1;
+    }
+
     auto parser = jp::Parser(tokens);
-    auto obj = parser.parse_object();
+    auto obj = parser.parse();
 
     if (!obj) {
         std::cerr << "Failed to parse object" << std::endl;
         return 1;
     }
 
-    for (const auto &[key, value] : *obj) {
-        std::cout << key << ": ...";
-    }
+    auto output = jp::to_string(*obj);
+    std::cout << output << std::endl;
 
     return 0;
 }
